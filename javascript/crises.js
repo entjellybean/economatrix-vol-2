d0da8b38af3065714ec890c4559716ea9c282d92
entjellybean <basakunal2003@gmail.com>
2025-03-07 15:43:12 +0100

    crises html

--- javascript/crises.js
+++ javascript/crises.js
@@ -1,11 +1,4 @@
-const grandeBtn = document.getElementById('grande-depression-btn');
-const mainElement = document.getElementsByTagName('main')[0];
-const titles = document.getElementById('titles');
-const debut = document.getElementById('debut');
-
-function startLevel(){
-    titles.style.display='none';
-    mainElement.style.display='grid';
-}
-grandeBtn.addEventListener('click',startLevel);
+function closeOverlay(overlayId) {
+    document.getElementsByClassName(overlayId).style.display = "none";
+  }
 
