df28b4125d1ca11c15e19fe11a165bec2a99ad5d
entjellybean <basakunal2003@gmail.com>
2025-03-07 20:52:40 +0100

    invest

--- javascript/crises.js
+++ javascript/crises.js
@@ -1,4 +1,37 @@
-function closeOverlay(overlayId) {
-    document.getElementsByClassName(overlayId).style.display = "none";
-  }
+function closeOverlay(className) {
+    const element = document.querySelector(`.${className}:not([style*="display: none"])`);
+    if (element) {
+        element.style.display = 'none';
+    }
+}
+function openOverlay(className) {
+    const element = document.querySelector(`.${className}:not([style*="display: none"])`);
+    if (element) {
+        element.style.display = 'grid';
+    }
+}
+
+function toggleSection(showId, hideId) {
+    const hideSection = document.getElementById(hideId);
+    if (hideSection) {
+        hideSection.style.display = 'none';
+    }
+
+    const showSection = document.getElementById(showId);
+    if (showSection) {
+        showSection.style.display = 'grid';
+    }
+}
+
+function toggleClass(showClass, hideClass) {
+    const hideSection = document.querySelector(`.${hideClass}:not([style*="display: none"])`);
+    if (hideSection) {
+        hideSection.style.display = 'none';
+    }
+
+    const showSection = document.querySelector(`.${showClass}:not([style*="display: none"])`);
+    if (showSection) {
+        showSection.style.display = 'grid';
+    }
+}
 
